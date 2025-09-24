298d4b2eb2b064b4f7c3345b5e007fe35253d38d
gwozdziu1017 <gwozdziu1017@o2.pl>
2025-09-24 09:11:10 +0200

    Initial commit

---
+++ Timer/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by Damian Gwóźdź on 24/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Damian Gwóźdź on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
